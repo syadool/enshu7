1d9d8b5cb318743a261b30bf5d65ccf20bb62889
syadool <takerumorimoto0417@gmail.com>
2022-07-11 11:31:51 +0900

    2022/7/11

--- practice11_1/main.cpp
+++ practice11_1/main.cpp
@@ -4,7 +4,7 @@ int main() {
     int test;
     printf("点数を入力");
     scanf("%d", &test);
-    if(test < 0 && test > 100){
+    if(test < 0 || test > 100){
         printf("無効な点数");
     }
     return 0;
